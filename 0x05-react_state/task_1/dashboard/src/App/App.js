dcb64ca05db8c22d13edc6e888cdabf705016e33
Phumlani Mabophe <93977501+phumlanimabophe@users.noreply.github.com>
2024-02-23 03:56:04 +0000

    done

--- 0x05-react_state/task_1/dashboard/src/App/App.js
+++ 0x05-react_state/task_1/dashboard/src/App/App.js
@@ -10,25 +10,27 @@ import { getLatestNotification } from '../utils/utils';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
-
+// Main application component
 class App extends React.Component {
-  
   constructor(props) {
     super(props);
+
+    // Properties and state initialization
     this.isLoggedIn = props.isLoggedIn;
     this.logOut = props.logOut;
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.listCourses = [
-      {id: 1, name: 'ES6', credit: 60},
-      {id: 2, name: 'Webpack', credit: 20},
-      {id: 3, name: 'React', credit: 40}
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 }
     ];
-  
+
     this.listNotifications = [
-      {id: 1, value: "New course available", type: "default"},
-      {id: 2, value: "New resume available", type: "urgent"},
-      {id: 3, html: {__html: getLatestNotification()}, type: "urgent"},
+      { id: 1, value: "New course available", type: "default" },
+      { id: 2, value: "New resume available", type: "urgent" },
+      { id: 3, html: { __html: getLatestNotification() }, type: "urgent" },
     ];
+
     this.state = {
       displayDrawer: false
     };
@@ -37,35 +39,41 @@ class App extends React.Component {
     this.handleHideDrawer = this.handleHideDrawer.bind(this);
   }
 
+  // Handles the display of the drawer
   handleDisplayDrawer() {
     this.setState({
       displayDrawer: true
     });
   }
 
+  // Handles the hiding of the drawer
   handleHideDrawer() {
     this.setState({
       displayDrawer: false
     });
   }
 
+  // Handles keydown event for a specific key combination
   handleKeyDown(e) {
     if (e.ctrlKey && e.key === 'h') {
       e.preventDefault();
       alert("Logging you out");
       this.logOut();
-    }  
+    }
   }
 
+  // Lifecycle method to add event listener for keydown
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
   }
 
+  // Lifecycle method to remove event listener when the component unmounts
   componentWillUnmount() {
     window.removeEventListener('keydown', this.handleKeyDown);
   }
 
-  render () {
+  // Renders the App component
+  render() {
     return (
       <React.Fragment>
         <Notification
@@ -77,8 +85,8 @@ class App extends React.Component {
         <div className={css(bodyStyles.App)}>
           <Header />
           {this.props.isLoggedIn ?
-            <BodySectionWithMarginBottom title="Course list"><CourseList listCourses={this.listCourses}/></BodySectionWithMarginBottom>
-          : 
+            <BodySectionWithMarginBottom title="Course list"><CourseList listCourses={this.listCourses} /></BodySectionWithMarginBottom>
+            :
             <BodySectionWithMarginBottom title="Log in to continue"><Login /></BodySectionWithMarginBottom>
           }
           <BodySection title="News from the School">
@@ -93,6 +101,7 @@ class App extends React.Component {
   }
 }
 
+// Aphrodite styles for the component
 const bodyStyles = StyleSheet.create({
   App: {
     position: 'relative',
@@ -101,20 +110,21 @@ const bodyStyles = StyleSheet.create({
 });
 
 const footerStyles = StyleSheet.create({
-	footer: {
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderTop: '3px solid #E11D3F',
-		padding: '1rem',
-		fontStyle: 'italic',
-	}
+  footer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTop: '3px solid #E11D3F',
+    padding: '1rem',
+    fontStyle: 'italic',
+  }
 });
 
+// Default props and propTypes for the App component
 App.defaultProps = {
   isLoggedIn: false,
-  logOut: () => {}
+  logOut: () => { }
 };
 
 App.propTypes = {
--- 0x05-react_state/task_1/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_1/dashboard/src/Footer/Footer.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { getFooterCopy, getFullYear } from '../utils/utils';
 
+/**
+ * Footer Component:
+ * This functional component represents the footer section of the application.
+ * It displays copyright information along with the current year and footer text.
+ *
+ * @returns {JSX.Element} - React component
+ */
 function Footer() {
   return (
     <div className="App-footer">
       <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
     </div>
-  )
+  );
 }
 
 export default Footer;
